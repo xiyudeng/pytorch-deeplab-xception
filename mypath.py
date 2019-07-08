a624bc319619ddf9333158850a93ccb3d50cf743
xiyudeng <52501352+xiyudeng@users.noreply.github.com>
2019-07-08 10:40:20 +0200

    Update mypath.py

--- mypath.py
+++ mypath.py
@@ -2,13 +2,13 @@ class Path(object):
     @staticmethod
     def db_root_dir(dataset):
         if dataset == 'pascal':
-            return '/path/to/datasets/VOCdevkit/VOC2012/'  # folder that contains VOCdevkit/.
+            return 'dataloaders/datasets/pascal.py'  # folder that contains VOCdevkit/.
         elif dataset == 'sbd':
-            return '/path/to/datasets/benchmark_RELEASE/'  # folder that contains dataset/.
+            return 'dataloaders/datasets/sbd.py'  # folder that contains dataset/.
         elif dataset == 'cityscapes':
-            return '/path/to/datasets/cityscapes/'     # foler that contains leftImg8bit/
+            return 'dataloaders/datasets/cityscapes.py'     # foler that contains leftImg8bit/
         elif dataset == 'coco':
-            return '/path/to/datasets/coco/'
+            return 'dataloaders/datasets/coco.py' 
         else:
             print('Dataset {} not available.'.format(dataset))
             raise NotImplementedError
